ae5e2f0d1314ad8ed7735c531b1daffa2f475dd4
Michael <michaelbrooks6897@gmail.com>
2024-02-10 21:27:58 -0500

    Update pages, add about me

--- my-skeleton-app/src/routes/+layout.svelte
+++ my-skeleton-app/src/routes/+layout.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
 	import '../app.postcss';
-	import { AppShell, AppBar, initializeStores, Drawer, getDrawerStore } from '@skeletonlabs/skeleton';
+	import { AppShell, AppBar, initializeStores, Drawer, getDrawerStore, Avatar } from '@skeletonlabs/skeleton';
   import Navigation from '$lib/Navigation/Navigation.svelte';
 	
 	initializeStores();
@@ -23,19 +23,33 @@
 		<AppBar>
 			<svelte:fragment slot="lead">
 				<div class="flex items-center">
-					<button class="lg:hidden btn btn-sm mr-4" on:click="{drawerOpen}">
-							<span>
-									<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
-											<rect width="100" height="20" />
-											<rect y="30" width="100" height="20" />
-											<rect y="60" width="100" height="20" />
-									</svg>
-							</span>
-					</button>
+					<div class="flex w-full">
+						<button class="lg:hidden btn btn-sm mr-4" on:click="{drawerOpen}">
+								<span>
+										<svg viewBox="0 0 100 80" class="fill-token w-4 h-4">
+												<rect width="100" height="20" />
+												<rect y="30" width="100" height="20" />
+												<rect y="60" width="100" height="20" />
+										</svg>
+								</span>
+						</button>
+						<strong class="text-xl uppercase">Michael Brooks</strong>
+					</div>
 				</div>
-				<strong class="text-xl uppercase">Michael Brooks</strong>
 			</svelte:fragment>
 			<svelte:fragment slot="trail">
+					<div class="flex">
+						<a target="_blank" href="https://github.com/michaelbrooks1234">
+							<button class="btn p-2">
+								<Avatar width='w-10' src="/githubIcon.png"/>	
+							</button>
+						</a>
+						<a target="_blank" href="https://www.linkedin.com/in/michael-brooksengineer/">
+							<button class="btn p-2">
+								<Avatar width='w-10' src="/linkedinIcon.png"/>	
+							</button>
+						</a>
+					</div>
 			</svelte:fragment>
 		</AppBar>
 	</svelte:fragment>
--- my-skeleton-app/src/routes/about/+page.svelte
+++ my-skeleton-app/src/routes/about/+page.svelte
@@ -1,23 +1,57 @@
+<div class="flex w-full h-1/10 justify-center mt-10">
+  <h1 class="text-5xl text-primary-300">About Me!</h1>
+</div>
 <div class="flex flex-col h-full w-full justify-start md:justify-center items-center">
   <div class="w-full md:w-3/4 h-3/4 px-2 lg:px-0">
     <div class="flex w-full h-1/2 justify-center items-center md:justify-start md:items-start mt-10 md:mt-0">
-      <div class="card p-4 rounded-xl w-96 lg:w-1/2 h-full">
-
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+        <div class="card-header">
+          <span class="text-2xl">Self Taught</span>
+        </div>
+        <div class="p-4">
+          <span class="text-base">
+            As I mentioned on the landing page, I am a self taught software engineer. I am very passionate about my work and I am proud to have made it into the field.
+            I started out by learning Python and swiftly moved on to practicing data structure & algorithm coding problems while using C++.
+            I then moved on to learning practical applications by focusing on web development and making web apps.
+          </span>
+        </div>
       </div>
     </div>
     <div class="flex w-full h-1/2 justify-center items-center md:justify-end md:items-start mt-10">
-      <div class="card p-4 rounded-xl w-96 lg:w-1/2 h-full">
-
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+        <div class="card-header">
+          <span class="text-2xl">Professional</span>
+        </div>
+        <div class="p-4">
+          <span class="text-base">
+            While working my dayjobs I was also working on improving my qualifications as a SWE, this paid off by landing me my position as a client success engineer at Chili Publish.
+            I was able to really focus on bettering 
+          </span>
+        </div>
       </div>
     </div>
     <div class="flex w-full h-1/2 justify-center items-center md:justify-start md:items-start mt-10">
-      <div class="card p-4 rounded-xl w-96 lg:w-1/2 h-full">
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+        <div class="card-header">
+          <span class="text-2xl">Self Taught</span>
+        </div>
+        <div class="p-4">
+          <span class="text-base">
 
+          </span>
+        </div>
       </div>
     </div>
-    <div class="flex w-full h-1/2 justify-center items-center md:justify-end md:items-start mt-10 mb-10">
-      <div class="card p-4 rounded-xl w-96 lg:w-1/2 h-full">
+    <div class="flex  w-full h-1/2 justify-center items-center md:justify-end md:items-start mt-10 mb-10">
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+        <div class="card-header">
+          <span class="text-2xl">Self Taught</span>
+        </div>
+        <div class="p-4">
+          <span class="text-base">
 
+          </span>
+        </div>
       </div>
     </div>
   </div>
