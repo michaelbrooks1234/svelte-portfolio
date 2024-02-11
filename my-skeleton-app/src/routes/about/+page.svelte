1fa5ab880778708c149c46c692deba0c5216c531
Michael <michaelbrooks6897@gmail.com>
2024-02-10 22:01:58 -0500

    add content and adjust styling

--- my-skeleton-app/src/routes/about/+page.svelte
+++ my-skeleton-app/src/routes/about/+page.svelte
@@ -1,12 +1,12 @@
-<div class="flex w-full h-1/10 justify-center mt-10">
+<div class="flex w-full h-fit justify-center mt-10">
   <h1 class="text-5xl text-primary-300">About Me!</h1>
 </div>
-<div class="flex flex-col h-full w-full justify-start md:justify-center items-center">
+<div class="flex flex-col h-fit mt-10 w-full justify-start md:justify-center items-center">
   <div class="w-full md:w-3/4 h-3/4 px-2 lg:px-0">
-    <div class="flex w-full h-1/2 justify-center items-center md:justify-start md:items-start mt-10 md:mt-0">
-      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+    <div class="flex w-full h-1/3 justify-center items-center md:justify-start md:items-start mt-10 md:mt-0">
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-3/5 h-fit">
         <div class="card-header">
-          <span class="text-2xl">Self Taught</span>
+          <span class="text-2xl text-primary-600">Self Taught</span>
         </div>
         <div class="p-4">
           <span class="text-base">
@@ -17,39 +17,44 @@
         </div>
       </div>
     </div>
-    <div class="flex w-full h-1/2 justify-center items-center md:justify-end md:items-start mt-10">
-      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+    <div class="flex w-full h-1/3 justify-center items-center md:justify-end md:items-start mt-10">
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-3/5 h-fit">
         <div class="card-header">
-          <span class="text-2xl">Professional</span>
+          <span class="text-2xl text-primary-600">Professional</span>
         </div>
         <div class="p-4">
           <span class="text-base">
             While working my dayjobs I was also working on improving my qualifications as a SWE, this paid off by landing me my position as a client success engineer at Chili Publish.
-            I was able to really focus on bettering 
+            I was able to really focus on improving my wed development skills, especially with network requests and APIs. After my time at Chili Publish I became a Software Developer
+            at Sekady. Here, I had the chance to focus on what I really wanted which was building out new features across the frontend and the backend.
           </span>
         </div>
       </div>
     </div>
-    <div class="flex w-full h-1/2 justify-center items-center md:justify-start md:items-start mt-10">
-      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+    <div class="flex w-full h-1/3 justify-center items-center md:justify-start md:items-start mt-10">
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-3/5 h-fit">
         <div class="card-header">
-          <span class="text-2xl">Self Taught</span>
+          <span class="text-2xl text-primary-600">Personal Projects</span>
         </div>
         <div class="p-4">
           <span class="text-base">
-
+            Now I have the skills and experience to build out entire web applications and I plan on seeing what I can create using this knowledge, as well as just improving as a Software Engineer.
+            First things first is creating a well built portfolio to showcase my awesome (hopefully) work. I am not a designer, so hopefully I can manage to make everything look some what nice.
+            I also have a few ideas for practical apps that will hopefully be good enough to monetize, but that's just a bonus.
           </span>
         </div>
       </div>
     </div>
-    <div class="flex  w-full h-1/2 justify-center items-center md:justify-end md:items-start mt-10 mb-10">
-      <div class="card card-hover p-4 rounded-xl w-96 lg:w-1/2 h-full">
+    <div class="flex  w-full h-1/3 justify-center items-center md:justify-end md:items-start mt-10 mb-10">
+      <div class="card card-hover p-4 rounded-xl w-96 lg:w-3/5 h-fit">
         <div class="card-header">
-          <span class="text-2xl">Self Taught</span>
+          <span class="text-2xl text-primary-600">End Goal</span>
         </div>
         <div class="p-4">
           <span class="text-base">
-
+            I have a single end goal and that is to become a very good Software Engineer. I have a few things that I want to excel at specifically. #1 would be having job specific knowledge.
+            I believe that I have already made leaps and bounds in this regards due to my professional experience, but I want to take a deeper dive on the technologies that I use to build out applications.
+            End goal #2 would definitely be getting very good with Data Structures and Algorithms. This is just something I want to be good at. Here is some people that inspire me:
           </span>
         </div>
       </div>
